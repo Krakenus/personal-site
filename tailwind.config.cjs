a219fa2d6a8e61d373768a90ca9472f5f4fa8965
Milan Vlasák <krakenus02@gmail.com>
2022-05-10 19:32:06 +0200

    google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
