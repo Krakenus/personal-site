1ca060e0e35596de24252beaf02b0f7ee187daf7
Milan Vlasák <krakenus02@gmail.com>
2022-05-19 22:09:04 +0200

    avatar

--- src/lib/components/page_header.svelte
+++ src/lib/components/page_header.svelte
@@ -1,8 +1,13 @@
-<h1>Personal Site</h1>
-<p>avatar here</p>
-<p><strong>Welcome to my personal website!</strong></p>
+<script lang="ts">
+    import avatar from '$lib/assets/avatar.jpg';
+</script>
+
+<div class="my-2">
+    <img src="{avatar}" alt="avatar" class="inline rounded-full border-8 border-gray-300 w-56">
+</div>
+<p class="font-semibold">Welcome to my personal website!</p>
 <nav>
-    <small>Here you can find:</small>
+    <p>Here you can find:</p>
     <div class="flex flex-col leading-loose max-w-3xl mx-auto my-2 sm:flex-row sm:justify-around">
         <div><a href="/">Something about me</a></div>
         <div><a href="/technologies">Technologies I like and use</a></div>
