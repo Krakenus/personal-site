7e9911c112424cef386bfb44dcc563006ff13d23
Milan Vlasák <krakenus02@gmail.com>
2022-06-10 15:59:19 +0200

    navigation with active link

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,8 +1,12 @@
+<script lang="ts">
+    import Navlink from '$lib/components/navlink.svelte';
+</script>
+
 <nav>
     <p>Here you can find:</p>
     <div class="flex flex-col leading-loose max-w-3xl mx-auto my-2 sm:flex-row sm:justify-around">
-        <div><a class="no-underline hover:underline" href="/">Something about me</a></div>
-        <div><a class="no-underline hover:underline" href="/technologies">Technologies I like and use</a></div>
-        <div><a class="no-underline hover:underline" href="/contact">Ways how to reach me</a></div>
+        <Navlink href="/">Something about me</Navlink>
+        <Navlink href="/technologies">Technologies I like and use</Navlink>
+        <Navlink href="/contact">Ways how to reach me</Navlink>
     </div>
 </nav>
---
+++ src/lib/components/navlink.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+
+    export let href: string;
+</script>
+
+<div>
+    <a {href} class:text-gray-600={$page.url.pathname === href} class="no-underline"><slot /></a>
+</div>
