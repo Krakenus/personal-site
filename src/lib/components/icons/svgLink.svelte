2c0f218f90ed158e16c4838f80ea54f5c1d90d50
Milan Vlasák <krakenus02@gmail.com>
2022-07-04 21:43:16 +0200

    contact page

---
+++ src/lib/components/icons/svgLink.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let href: string;
+    export let src: string;
+    export let title: string;
+</script>
+
+<div>
+    <a {href}>
+        <img class="w-24 sm:w-32" {src} alt={title} {title}>
+    </a>
+</div>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,6 +1,23 @@
+<script lang="ts">
+    import SvgLink from '$lib/components/icons/svgLink.svelte';
+    import IconRow from '$lib/components/icons/iconRow.svelte';
+</script>
+
 <svelte:head>
     <title>How to reach me</title>
 </svelte:head>
 
 
 <h1>How to reach me</h1>
+
+<IconRow>
+    <SvgLink href="https://github.com/Krakenus"
+             title="GitHub"
+             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+    <SvgLink href="https://www.linkedin.com/in/milan-vlasak-cz"
+             title="LinkedIn"
+             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" />
+    <SvgLink href="https://twitter.com/Krakenus"
+             title="Twitter"
+             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" />
+</IconRow>
