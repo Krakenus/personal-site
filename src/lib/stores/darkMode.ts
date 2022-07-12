e23a2e0aeaef5041664bbb2585f13886e47cbb8b
Milan Vlasák <krakenus02@gmail.com>
2022-07-12 11:07:32 +0200

    dark mode switching improved

--- src/lib/stores/darkMode.ts
+++ src/lib/stores/darkMode.ts
@@ -2,24 +2,35 @@ import { server } from '$app/env';
 import { writable } from 'svelte/store';
 
 function createDarkModeStore() {
-    const initialValue: boolean = server ?
-                     false :
-                     (localStorage.darkMode || (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches));
+    const systemThemeDark = server ? false : window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userTheme = server ? undefined : localStorage.theme;
+    const initialValue = userTheme === 'dark' || (userTheme === undefined && systemThemeDark);
 
+    const switchDarkModeClass = (enabled: boolean) => {
+        if(enabled) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }
+
+    if(!server) {
+        switchDarkModeClass(initialValue);
+    }
     const { subscribe, set, update } = writable<boolean>(initialValue);
     return {
         subscribe,
         toggle: () => update((value: boolean) => {
             const newValue = !value;
-            if(newValue) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-            localStorage.darkMode = newValue;
+            switchDarkModeClass(newValue);
+            localStorage.theme = newValue ? 'dark' : 'light';
             return newValue;
         }),
-        reset: () => set(initialValue)
+        reset: () => {
+            switchDarkModeClass(systemThemeDark);
+            localStorage.removeItem('theme');
+            set(systemThemeDark);
+        }
     }
 }
 
