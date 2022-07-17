8c10039e25d4c359deee9360e4dda4938e257e38
Milan Vlasák <krakenus02@gmail.com>
2022-07-17 21:24:38 +0200

    dark mode contact form styling

--- src/lib/components/forms/contactForm.svelte
+++ src/lib/components/forms/contactForm.svelte
@@ -3,11 +3,14 @@
     import FormTextarea from '$lib/components/forms/formTextarea.svelte';
 </script>
 
-<div>
+<div class="mb-4">
     <form>
         <FormInput name="name" type="text" label="Name" required={true} placeholder="Enter your name" />
         <FormInput name="email" type="email" label="E-mail" required={true} placeholder="Enter your e-mail" />
         <FormTextarea name="message" label="Message" required={true} placeholder="Enter your message" />
-        <button class="bg-blue-500 hover:bg-blue-700 p-2 px-3 mx-0.5 text-center rounded-md transition-colors duration-300 ease-in-out text-white" type="submit">Send</button>
+        <div>
+            <button class="float-right bg-blue-500 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-900 p-2 px-3 mx-0.5 text-center rounded-md transition-colors duration-300 ease-in-out text-white" type="submit">Send</button>
+            <div class="clear-both"></div>
+        </div>
     </form>
 </div>
