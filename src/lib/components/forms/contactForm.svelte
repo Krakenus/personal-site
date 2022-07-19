b8a02359d0eba14c70a5f55e1192c47e0946bcd5
Milan Vlasák <krakenus02@gmail.com>
2022-07-19 08:14:08 +0200

    submit handler in contact form

--- src/lib/components/forms/contactForm.svelte
+++ src/lib/components/forms/contactForm.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
     import FormInput from '$lib/components/forms/formInput.svelte';
     import FormTextarea from '$lib/components/forms/formTextarea.svelte';
+
+    function handleSubmit(event: SubmitEvent) {
+        const formData = new FormData(event.target as HTMLFormElement);
+        fetch('/api/v1/send-email', {
+            method: 'POST',
+            body: formData,
+        });
+    }
 </script>
 
 <div class="mb-4">
-    <form>
+    <form on:submit|preventDefault={handleSubmit}>
         <FormInput name="name" type="text" label="Name" required={true} placeholder="Enter your name" />
         <FormInput name="email" type="email" label="E-mail" required={true} placeholder="Enter your e-mail" />
         <FormTextarea name="message" label="Message" required={true} placeholder="Enter your message" />
