2b54bdbc9edad907aee5e040423786478ce42bc8
Milan Vlasák <krakenus02@gmail.com>
2022-07-21 14:35:23 +0200

    email using mailchannels

--- src/routes/api/v1/send-email.ts
+++ src/routes/api/v1/send-email.ts
@@ -2,7 +2,39 @@
 export async function POST({ request }) {
   const data = await request.formData();
 
-  
+  const mailRequest = new Request('https://api.mailchannels.net/tx/v1/send', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [{email: import.meta.env.VITE_EMAIL_TO, name: import.meta.env.VITE_NAME_TO}]
+        }
+      ],
+      from: {
+        email: import.meta.env.VITE_EMAIL_FROM,
+        name: import.meta.env.VITE_NAME_FROM
+      },
+      subject: 'Website message',
+      content: [
+        {
+          type: 'text/plain',
+          value: `New website message.\n\nName: ${data.get('name')}\nE-mail: ${data.get('email')}\n\nMessage:\n${data.get('message')}`
+        },
+        {
+          type: 'text/html',
+          value: `New website message.<br><br>Name: ${data.get('name')}<br>E-mail: ${data.get('email')}<br><br>Message:<br>${data.get('message')}`
+        }
+      ]
+    })
+  });
+  fetch(mailRequest).then((response: Response) => {
+    console.log(response);
+  }).catch(reason => {
+    console.log(reason);
+  });
 
   return {
     status: 200,
