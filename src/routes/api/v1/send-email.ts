c06a5b1473ab32fb91a49d0b63132e59a46d0117
Milan Vlasák <krakenus02@gmail.com>
2022-07-21 13:31:59 +0200

    nodemailer removed

--- src/routes/api/v1/send-email.ts
+++ src/routes/api/v1/send-email.ts
@@ -1,25 +1,8 @@
-import {createTransport} from 'nodemailer';
-
 /** @type {import('./__types/items').RequestHandler} */
 export async function POST({ request }) {
   const data = await request.formData();
 
-  const mailer = createTransport({
-    host: import.meta.env.VITE_EMAIL_HOST,
-    port: import.meta.env.VITE_EMAIL_PORT,
-    auth: {
-      user: import.meta.env.VITE_EMAIL_USERNAME,
-      pass: import.meta.env.VITE_EMAIL_PASSWORD
-    }
-  })
-
-  await mailer.sendMail({
-    from: '',
-    to: '',
-    subject: 'New message from personal site',
-    text: `${data.get("name")}\n${data.get("email")}\n\n${data.get("message")}`,
-    html: `${data.get("name")}<br>${data.get("email")}<br><br>${data.get("message")}`
-  })
+  
 
   return {
     status: 200,
