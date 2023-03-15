8d3b50be622b90c33829896cc2dcad07e1ee3205
Milan Vlasák <krakenus02@gmail.com>
2023-03-15 14:10:57 +0100

    tech heading changed

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -5,7 +5,7 @@
 <nav>
     <div class="grid grid-cols-1 sm:grid-cols-3 leading-loose mx-auto my-2 justify-center justify-items-center content-center items-center">
         <Navlink href="/">About me</Navlink>
-        <Navlink href="/technologies">Technologies I like</Navlink>
+        <Navlink href="/technologies">My tech stack</Navlink>
         <Navlink href="/contact">How to reach me</Navlink>
     </div>
 </nav>
--- src/routes/technologies/+page.svelte
+++ src/routes/technologies/+page.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <svelte:head>
-    <title>Milan Vlasák | Technologies I like</title>
-    <meta property="og:title" content="Technologies I like">
+    <title>Milan Vlasák | My tech stack</title>
+    <meta property="og:title" content="My tech stack">
     <meta name="description" property="og:description" content="List of most of the technologies I use. The list is not exhaustive but it covers my usual stack.">
 
     <link rel="canonical" href="https://milanvlasak.cz/technologies"/>
 </svelte:head>
 
-<h1>Technologies I like</h1>
+<h1>My tech stack</h1>
 
 <p>Here you can find most of the technologies I use. The list is not exhaustive but it covers my usual stack.</p>
 
