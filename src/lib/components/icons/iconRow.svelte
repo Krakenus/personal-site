d769a9fd659d5af40cd2815a7f0013647bed4bee
Milan Vlasák <krakenus02@gmail.com>
2023-07-05 23:31:50 +0200

    footer updated

--- src/lib/components/icons/iconRow.svelte
+++ src/lib/components/icons/iconRow.svelte
@@ -1,3 +1,3 @@
-<div class="bordered-content flex flex-row flex-wrap justify-evenly justify-items-center bg-white dark:bg-gray-600 py-2">
+<div class="bordered-content flex flex-row flex-wrap justify-evenly justify-items-center bg-white dark:bg-gray-600 my-2 py-4">
     <slot />
 </div>
