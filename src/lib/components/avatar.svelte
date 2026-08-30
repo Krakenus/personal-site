377d9ba1c565ba7cd49d06986dd5d06135ef1632
Milan Vlasák <krakenus02@gmail.com>
2026-08-30 09:44:44 +0200

    avatar image moved

--- src/lib/components/avatar.svelte
+++ src/lib/components/avatar.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    import avatar from '$lib/assets/avatar.jpg';
-</script>
-
 <div class="my-2">
-    <img src="{avatar}" alt="avatar" class="inline rounded-full w-56">
+    <img src="/avatar.jpg" alt="avatar" class="inline rounded-full w-56">
 </div>
